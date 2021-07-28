d4292f04a8497c7f10ee0c662c611203e56853e6
Artem Yemelyanov <ae1666528@gmail.com>
2021-07-28 17:01:02 +0300

    Updates

---
+++ precache-manifest.616afc5dfeb1cad2507ffaa7ac5a32d8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dae7fa9ecd44a366b3772a1bad332d9",
+    "url": "/react_tabs-with-router/index.html"
+  },
+  {
+    "revision": "6fc852c3a4e1a627364d",
+    "url": "/react_tabs-with-router/static/css/2.1890a892.chunk.css"
+  },
+  {
+    "revision": "72fc32a6ba4edd2fecf3",
+    "url": "/react_tabs-with-router/static/css/main.10b612de.chunk.css"
+  },
+  {
+    "revision": "6fc852c3a4e1a627364d",
+    "url": "/react_tabs-with-router/static/js/2.d1d7ba27.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/react_tabs-with-router/static/js/2.d1d7ba27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72fc32a6ba4edd2fecf3",
+    "url": "/react_tabs-with-router/static/js/main.23ff68a1.chunk.js"
+  },
+  {
+    "revision": "b3a37dc3087f54d4e458",
+    "url": "/react_tabs-with-router/static/js/runtime-main.90b2c16e.js"
+  }
+]);
